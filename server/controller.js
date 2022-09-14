b4d4128987c43fae18268af1a709d5c97593f433
Adam Cox <adamcox.dev@gmail.com>
2022-09-14 15:57:38 -0600

    Final commit for project

--- server/controller.js
+++ server/controller.js
@@ -31,10 +31,10 @@ module.exports = {
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`*****YOUR CODE HERE*****
-        
-        insert into cc_emp_appts (emp_id, appt_id)
-        values (${nextEmp}, ${apptId}),
+        sequelize.query(`UPDATE cc_appointments SET
+        approved = true WHERE appt_Id = '${apptId}';
+        INSERT INTO cc_emp_appts (emp_id, appt_id)
+        VALUES (${nextEmp}, ${apptId}),
         (${nextEmp + 1}, ${apptId});
         `)
             .then(dbRes => {
@@ -54,12 +54,21 @@ module.exports = {
         .catch(err => console.log(err))
     },
     getPastAppointments: (req, res) => {
-        sequelize.query(`SELECT a.appt_id, a.date, a.service_type, u.first_name, u.last_name 
-        FROM cc_appointments a
-        JOIN cc_emp_appts ea on a.appt_id = ea.appt_id
-        JOIN cc_employees e on e.emp_id = ea.emp_id
-        JOIN cc_users u on e.user_id = u.user_id
-        WHERE a.approved = true AND a.completed = true
-        order by a.date desc; `)
-    }
+        sequelize.query(`select a.appt_id, a.date, a.service_type, a.approved, a.completed, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = true
+        order by a.date desc;`)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+        },
+        completeAppointment: (req, res) => {
+            sequelize.query(`update cc_appointments
+            set completed = true
+            where appt_id = '${req.body.apptId}';`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+        }
 }
